d93b3764f67cd346cc898862799fa2739d7b2947
natashka2201 <natashka2201@gmail.com>
2025-06-13 16:03:56 +0000

    12

--- apps/backend/src/lib/db.ts
+++ apps/backend/src/lib/db.ts
@@ -1,6 +1,6 @@
+// apps/backend/src/lib/db.ts
 import { initializeApp, applicationDefault } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-initializeApp({ credential: applicationDefault() });
-
+initializeApp({ credential: applicationDefault() }); // без databaseURL!
 export const db = getFirestore();

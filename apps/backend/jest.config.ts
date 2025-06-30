7fa99a98f38d41681eda34b4be7ba6d58696af96
natashka2201 <natashka2201@gmail.com>
2025-06-30 17:03:08 +0000

    11111

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,5 +1,22 @@
-export default {
+// apps/backend/jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+
+  // Пути до тестов
   testMatch: ['**/tests/**/*.test.ts'],
+
+  globals: {
+    'ts-jest': {
+      // Указываем Jest-у, какой tsconfig брать
+      tsconfig: '<rootDir>/tsconfig.jest.json'
+    }
+  },
+
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['src/**/*.{ts,js}'],
 };
+
+export default config;
